838a1b37c808868dce39010e5fa847f4e1688e6b
Mohammad Umer <mohammadumershaikh9@gmail.com>
2026-09-07 00:22:29 +0530

    Clean up server.js by removing comments and fixing newline
    Removed commented-out line and added a newline at the end of the file.

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// server.js
-
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -40,3 +38,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
